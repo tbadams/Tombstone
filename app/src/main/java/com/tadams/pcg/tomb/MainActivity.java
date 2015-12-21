ab0f819a940fd7b31f25b44ff7be7033a3d43f09
Trevor Adams <trevorbadams@gmail.com>
2015-12-20 23:09:54 -0800

    Move tombstone display code to tombstone fragment. Activity no coordinated two fragments actions.

--- app/src/main/java/com/tadams/pcg/tomb/MainActivity.java
+++ app/src/main/java/com/tadams/pcg/tomb/MainActivity.java
@@ -5,12 +5,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -22,10 +17,11 @@ import com.tadams.pcg.tomb.model.DeathSeed;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements DeathsFragment.DeathListProvider{
+public class MainActivity extends AppCompatActivity implements DeathsFragment.DeathListProvider,
+        TombstoneFragment.DeathCreator {
 
     private static final String DEATHS_KEY = "deathslist";
-    private static final int GRAVE_WIDTH = 16;
+
 
     private ArrayList<CharDeath> deathList = new ArrayList<>();
 
@@ -45,30 +41,7 @@ public class MainActivity extends AppCompatActivity implements DeathsFragment.De
         }
 
         setContentView(R.layout.main);
-		final EditText nameEntry = (EditText)findViewById(R.id.char_name);
-		final Spinner charClassEntry = (Spinner)findViewById(R.id.char_class_spinner);
-		charClassEntry.setAdapter(new ArrayAdapter<>(this,
-                android.R.layout.simple_dropdown_item_1line, CharClass.values()));
-		Button genButton = (Button)findViewById(R.id.gen_button);
-		genButton.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(View p1) {
-                String name = nameEntry.getText().toString();
-                CharClass charClass = (CharClass) charClassEntry.getSelectedItem();
-                CharDeath death = new DeathFactory().getDeath(name, charClass);
-                deathList.add(death);
-                ListFragment deaths = (ListFragment)getFragmentManager().findFragmentById(R.id.deaths_fragment);
-                if(deaths != null && deaths.isAdded()) {
-                    ((ArrayAdapter<CharDeath>)deaths.getListAdapter()).notifyDataSetChanged();
-                }
-                ((TextView) findViewById(R.id.death_screen)).setText(getGraveString(death.toString()));
-                nameEntry.setText("");
-            }
-        });
-
-
-        ((TextView) findViewById(R.id.death_screen)).setText(getGraveString("Arthur Shitson died in a butt"));
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
@@ -92,42 +65,18 @@ public class MainActivity extends AppCompatActivity implements DeathsFragment.De
         return deathList;
     }
 
-    private String makeGraveLine(String text) {
-        if(text.length() <= GRAVE_WIDTH) {
-            StringBuilder sb = new StringBuilder(25);
-            int totalFiller = GRAVE_WIDTH - text.length();
-            int leftFiller = totalFiller / 2;
-            int rightfiller = totalFiller - leftFiller;
-            sb.append("| ");
-            addSpaces(sb, leftFiller);
-            sb.append(text);
-            addSpaces(sb, rightfiller);
-            sb.append(" |\n");
-            return sb.toString();
-        } else {
-            throw new IllegalArgumentException("too long");
-        }
-    }
 
-    private void addSpaces(StringBuilder sb, int x) {
-        for( int i = 0; i < x; i++) {
-            sb.append(' ');
+    @Override
+    public void makeDeath(String name, CharClass charClass) {
+        CharDeath death = new DeathFactory().getDeath(name, charClass);
+        deathList.add(death);
+        ListFragment deaths = (ListFragment)getFragmentManager().findFragmentById(R.id.deaths_fragment);
+        if(deaths != null && deaths.isAdded()) {
+            ((ArrayAdapter<CharDeath>)deaths.getListAdapter()).notifyDataSetChanged();
         }
-    }
-
-    private String getGraveString(String graveText) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("----------\n")
-                .append("/          \\\n")
-                .append("/    REST    \\\n")
-                .append("/      IN      \\\n")
-                .append("/     PEACE      \\\n")
-                .append("/                  \\\n");
-        while(graveText.length() > GRAVE_WIDTH) {
-            sb.append(makeGraveLine(graveText.substring(0, GRAVE_WIDTH)));
-            graveText = graveText.substring(GRAVE_WIDTH);
+        TombstoneFragment tombstone = (TombstoneFragment)getFragmentManager().findFragmentById(R.id.tombstone_fragment);
+        if(tombstone != null && tombstone.isAdded()) {
+            tombstone.setDeath(death);
         }
-        sb.append(makeGraveLine(graveText));
-        return sb.toString();
     }
 }
--- app/src/main/java/com/tadams/pcg/tomb/TombstoneFragment.java
+++ app/src/main/java/com/tadams/pcg/tomb/TombstoneFragment.java
@@ -6,14 +6,95 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.tadams.pcg.tomb.model.CharClass;
+import com.tadams.pcg.tomb.model.CharDeath;
 
 /**
  * Created by tadams on 12/17/15.
  */
 public class TombstoneFragment extends Fragment {
+
+    private static final int GRAVE_WIDTH = 16;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tombstone, container, false);
+        View v = inflater.inflate(R.layout.tombstone, container, false);
+        final DeathCreator activity = (DeathCreator)getActivity();
+
+        final EditText nameEntry = (EditText)v.findViewById(R.id.char_name);
+        final Spinner charClassEntry = (Spinner)v.findViewById(R.id.char_class_spinner);
+        charClassEntry.setAdapter(new ArrayAdapter<>(getActivity(),
+                android.R.layout.simple_dropdown_item_1line, CharClass.values()));
+        Button genButton = (Button)v.findViewById(R.id.gen_button);
+        genButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View p1) {
+                String name = nameEntry.getText().toString();
+                CharClass charClass = (CharClass) charClassEntry.getSelectedItem();
+                activity.makeDeath(name, charClass);
+                nameEntry.setText("");
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+    }
+
+    public void setDeath(CharDeath death) {
+        ((TextView)getView().findViewById(R.id.death_screen)).setText(getGraveString(death.toString()));
+    }
+
+    private String makeGraveLine(String text) {
+        if(text.length() <= GRAVE_WIDTH) {
+            StringBuilder sb = new StringBuilder(25);
+            int totalFiller = GRAVE_WIDTH - text.length();
+            int leftFiller = totalFiller / 2;
+            int rightfiller = totalFiller - leftFiller;
+            sb.append("| ");
+            addSpaces(sb, leftFiller);
+            sb.append(text);
+            addSpaces(sb, rightfiller);
+            sb.append(" |\n");
+            return sb.toString();
+        } else {
+            throw new IllegalArgumentException("too long");
+        }
+    }
+
+    private void addSpaces(StringBuilder sb, int x) {
+        for( int i = 0; i < x; i++) {
+            sb.append(' ');
+        }
+    }
+
+    private String getGraveString(String graveText) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("----------\n")
+                .append("/          \\\n")
+                .append("/    REST    \\\n")
+                .append("/      IN      \\\n")
+                .append("/     PEACE      \\\n")
+                .append("/                  \\\n");
+        while(graveText.length() > GRAVE_WIDTH) {
+            sb.append(makeGraveLine(graveText.substring(0, GRAVE_WIDTH)));
+            graveText = graveText.substring(GRAVE_WIDTH);
+        }
+        sb.append(makeGraveLine(graveText));
+        return sb.toString();
+    }
+
+    public interface DeathCreator {
+        void makeDeath(String name, CharClass charClass);
     }
 }
