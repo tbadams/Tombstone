99a04a4befa53f938ee8896f7e37f651eae70e24
Trevor Adams <trevorbadams@gmail.com>
2015-12-22 15:06:00 -0800

    Abortive WIP attempts to find a better way to define Dangers.

---
+++ app/src/main/java/com/tadams/pcg/tomb/DangerTemplate.java
@@ -0,0 +1,8 @@
+package com.tadams.pcg.tomb;
+
+/**
+ * Created by tadams on 12/22/15.
+ */
+public class  DangerTemplate {
+
+}
---
+++ app/src/main/java/com/tadams/pcg/tomb/model/Hazard.java
@@ -0,0 +1,7 @@
+package com.tadams.pcg.tomb.model;
+
+/**
+ * Created by tadams on 12/22/15.
+ */
+public class Hazard {
+}
