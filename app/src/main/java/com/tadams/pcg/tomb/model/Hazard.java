466cd721da7bc8f40239caa5b1a466943bc8f782
Trevor Adams <trevorbadams@gmail.com>
2016-01-17 21:25:31 -0800

    Add hazards with random names.

--- app/src/main/java/com/tadams/pcg/tomb/model/Hazard.java
+++ app/src/main/java/com/tadams/pcg/tomb/model/Hazard.java
@@ -8,22 +8,36 @@ import java.util.Random;
  */
 public class Hazard {
     private static final String DIED_OF = "died of";
+
+    private String[] magicTypes = new String[] {"fire", "ice", "lightning", "psychic energy", "poison",
+            "disintegration"};
+
+    private final NameAlgorithm magicAlgorithm = new NameAlgorithm() {
+        @Override
+        String getName(Random random, String typeName) {
+            return typeName + " of " + magicTypes[random.nextInt(magicTypes.length)];
+        }
+    };
     private DangerTemplate[] hazards = {
             new DangerTemplate("acidic corpse"),
             new DangerTemplate("alchemical blast"),
             new DangerTemplate("death ray"),
             new DangerTemplate("carnivorous bag"),
             new DangerTemplate("riding accident"),
-            new DangerTemplate("blast"), //TODO magic
-            new DangerTemplate("bolt"), // TODO magic
-            new DangerTemplate("psychic blast"),
+            new DangerTemplate("blast").setNameAlgorithm(magicAlgorithm),
+            new DangerTemplate("bolt").setNameAlgorithm(magicAlgorithm),
             new DangerTemplate("boiling potion"),
             new DangerTemplate("cadaver"),
             new DangerTemplate("iron ball collision"),
             new DangerTemplate("system shock"),
             new DangerTemplate("System Shock 2"),
             new DangerTemplate("petrification"),
-            new DangerTemplate("the wrath of god").setNoArticle(), // TODO gods
+            new DangerTemplate("the wrath of ").setNoArticle().setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    return typeName + God.random(random);
+                }
+            }),
             new DangerTemplate("overexertion").setNoArticle(),
             new DangerTemplate("strangulation").setNoArticle(),
             new DangerTemplate("suffocation").setNoArticle(),
@@ -44,12 +58,31 @@ public class Hazard {
             new DangerTemplate("their own magical blast").setVerbPhrase("caught in").setNoArticle(),
             new DangerTemplate("genocidal confusion").setNoArticle(),
             new DangerTemplate("gas cloud"),
-            new DangerTemplate("fell"), // TODO onto sink or into chasm, spike pit, pit, etc.
+            new DangerTemplate("fell").setVerbPhrase("fell into").setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    String[] fallables = new String[]{"pit", "pit of iron spikes", "chasm", "pit of lava",
+                            "bottomless pit", "pit of snakes", "pool of acid", "sausage grinder"};
+                    return fallables[random.nextInt(fallables.length)];
+                }
+            }),
             new DangerTemplate("falling rock"),
             new DangerTemplate("falling object"),
-            new DangerTemplate("exploding"), // TODO object: door, chest
+            new DangerTemplate("exploding").setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    String[] explodables =  new String[]{"door", "chest", "large box", "wand", "rune", "crystal ball",
+                            "ring", "potion"};
+                    return explodables[random.nextInt(explodables.length)];
+                }
+            }),
             new DangerTemplate("explosion"),
-            new DangerTemplate("elementary").setNoArticle(), // TODO physics or chemistry
+            new DangerTemplate("elementary").setNoArticle().setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    return typeName + " " + (random.nextBoolean() ? "physics" : "chemistry");
+                }
+            }),
             new DangerTemplate("electric shock"),
             new DangerTemplate("electric chair"),
             new DangerTemplate("drowned").setNoArticle().setVerbPhrase(""), // TODO location, monster
@@ -57,15 +90,29 @@ public class Hazard {
             new DangerTemplate("Medusa's gaze").setNoArticle().setVerbPhrase("petrified by"),
             new DangerTemplate("cursed throne"),
             new DangerTemplate("iron ball").setVerbPhrase("crunched in the head by"),
+            new DangerTemplate("iron ball").setVerbPhrase("dragged downstairs by"), // TODO Consolidate with above?
             new DangerTemplate("crashing into iron bars").setNoArticle(),
             new DangerTemplate("burned to death").setNoArticle().setVerbPhrase(""),
             new DangerTemplate("contaminated water").setNoArticle(),
-            new DangerTemplate("contact-posioned speelbook"),
+            new DangerTemplate("contact-posioned spellbook"),
             new DangerTemplate("colliding with the ceiling").setNoArticle(),
             new DangerTemplate("drawbridge").setVerbPhrase("crushed to death by"), // TODO collapsing, falling, exploding
             new DangerTemplate("food").setVerbPhrase("choked on their").setNoArticle(), // TODO food
-            new DangerTemplate("bumping into").setNoArticle(), //TODO bumps
-            new DangerTemplate("petrified").setVerbPhrase("petrified by"), // TODO by what, cock(wait no), cock corpse, egg, tasting, kicking, tinning, swallowing, riding, helping, touching
+            new DangerTemplate("bumping into").setNoArticle().setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    return DungeonFeature.values()[random.nextInt(DungeonFeature.values().length)].getWord();
+                }
+            }),
+            new DangerTemplate("petrified").setVerbPhrase("petrified by").setNoArticle()
+                    .setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    String[] stoners = new String[]{"a cockatrice", "a cockatrice corpse", "Medusa", "a cockatrice egg",
+                    "tasting cockatrice meat", "riding a cockatrice", "touching a cockatrice corpse bare handed"};
+                    return stoners[random.nextInt(stoners.length)];
+                }
+            }),
             new DangerTemplate("bear trap"),
             new DangerTemplate("exsanguination").setNoArticle(),
             new DangerTemplate("blood loss").setNoArticle(),
@@ -112,77 +159,63 @@ public class Hazard {
             new DangerTemplate("escaped").setNoArticle().setVerbPhrase(""),
             new DangerTemplate("touching an artifact").setNoArticle(), //TODO artifacts
             new DangerTemplate("teleporting into a wall").setNoArticle(),
-            
+            new DangerTemplate("quicksand").setNoArticle(),
+            new DangerTemplate("kicking").setVerbPhrase("killed by kicking").setNameAlgorithm(new NameAlgorithm() {
+                    @Override
+                    String getName(Random random, String typeName) {
+                            // TODO Add objects.
+                            return DungeonFeature.values()[random.nextInt(DungeonFeature.values().length)].getWord();
+                    }
+            }),
+            new DangerTemplate("their own").setNoArticle().setNameAlgorithm(magicAlgorithm),
+            new DangerTemplate("life drainage").setNoArticle(),
+            new DangerTemplate("magic missile"),
+            new DangerTemplate("molten lava").setNoArticle().setVerbPhrase("burned by"),
+            new DangerTemplate("thrown potion"),
+            new DangerTemplate("elevator"),
+            new DangerTemplate("touching the edge of the universe"),
+            new DangerTemplate("teleported out of the dungeon and fell to their death").setNoArticle().setVerbPhrase(""),
+            new DangerTemplate("self-genocide"),
+            new DangerTemplate("imperious order"),
+            new DangerTemplate("turned to slime").setNoArticle().setVerbPhrase(""),
+            new DangerTemplate("went to heaven prematurely").setNoArticle().setVerbPhrase(""),
+            new DangerTemplate("burning potion of oil"),
+            new DangerTemplate("rotted corpse").setVerbPhrase("poisoned by"), // TODO Particular corpses
+            new DangerTemplate("potion of acid"),
+            new DangerTemplate("residual undead turning effect"),
+            new DangerTemplate("touch of death"),
+            new DangerTemplate("saddled pony").setVerbPhrase("slipped while mounting"),
+            new DangerTemplate("boulder").setVerbPhrase("squished under"),
+            new DangerTemplate("unsuccessful polymorph"),
+            new DangerTemplate("zapped").setVerbPhrase("zapped themself with").setNameAlgorithm(new NameAlgorithm() {
+                    @Override
+                    String getName(Random random, String typeName) {
+                            return random.nextBoolean() ? "spell" : "wand";
+                    }
+            }),
+            new DangerTemplate("buring scroll"),
+            new DangerTemplate("scroll of fire").setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    return random.nextBoolean() ? "fire" : "earth";
+                }
+            }),
+            new DangerTemplate("dangerous winds").setNoArticle(),
+            new DangerTemplate("lethal injection").setNoArticle(),
+            new DangerTemplate("hanging by the neck until dead").setNoArticle(),
+            new DangerTemplate("bottomless pit"),
+            new DangerTemplate("the body of ").setNoArticle().setVerbPhrase("unwisely ate").setNameAlgorithm(new NameAlgorithm() {
+                @Override
+                String getName(Random random, String typeName) {
+                    String[] riders = new String[]{"Death", "Famine", "Pestilence", "War", "Ronnie"};
+                    return riders[random.nextInt(riders.length)];
+                }
+            }),
+            new DangerTemplate("kicking an exploding cockatrice corpse").setNoArticle(),
     };
-
-//    public enum HazardType {
-//        GAS_SPORE_EXPLOSION,
-//        IRON_BALL,
-//        STUCK_IN_CREATURE,
-//        KICKING,
-//        OWN_WEAPON,
-//        BEAR_TRAP,
-//        LIFE_DRAIN,
-//        MAGIC_MISSILE,
-//        MOLTEN_LAVA,
-//        PETRIFICATION,
-//        POISON,
-//        POTION, // acid, holy, unholy,
-//        RESIDUAL_UNDEAD_TURNING,
-//        REVERTING_UNHEALTHY_NORMAL,
-//        ROTTED_CORPSE,
-//        GENOCIDE_SCROLL,
-//        EARTH_SCROLL,
-//        FIRE_SCROLL,
-//        SELF_GENOCIDE,
-//        SITTING_SPIKE,
-//        SLIPPED_MOUNTING,
-//        SQUISHED_BOULDER,
-//        TELEPORT_FALL,
-//        TELEPORT_WALL,
-//        THROWN_POTION,
-//        TOUCH_EDGE_UNIVERSE,
-//        DEATH_TOUCH,
-//        SLIMED,
-//        FAILED_POLYMORPH,
-//        WIND_SWEPT,
-//        HEAVEN_EARLY,
-//        MAGICAL_HORN,
-//        ATE_DEATH,
-//        ZAPPED_SELF,
-//        ARRESTED,
-//        HANGED,
-//        LETHAL_INJECTION,
-//        ELEVATOR,
-//        BOTTOMLESS_PIT,
-    // kicking exploding cockatrice corpse??
-//
-//        final String name;
-//        private int monsterLevel;
-//
-//        Hazard(String name){
-//            this.name = name;
-//        }
-//
-//        String getName(Random random) {
-//            return name;
-//        }
-//
-//        @Override
-//        public String toString()
-//        {
-//            return name;
-//        }
-//    }
-
-    private String name;
 
     Danger getHazard(Random random, int dungeonLevel) {
         return hazards[random.nextInt(hazards.length)].build(random);
-    }
-
-    public String getName() {
-        return name;
     }
 
     DangerTemplate[] getTemplates() {
